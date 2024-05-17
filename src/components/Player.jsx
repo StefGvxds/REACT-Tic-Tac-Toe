144eb21f34d9cafda8e966fe0a1296d3771ac60f
Stefanos Gavouchidis <129869539+StefGvxds@users.noreply.github.com>
2024-05-17 10:54:19 +0300

    update

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,32 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(initialName);
+
+  function handleEditing() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <li>
-      <span className="player-name">{name}</span>
-      <span className="player-symbol">{symbol}</span>
-      <button>Edit</button>
+      <span className="player">
+        {!isEditing && <span className="player-name">{name}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={handleChange}
+          ></input>
+        )}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleEditing}>{isEditing ? "Save" : "Edit"}</button>
+      </span>
     </li>
   );
 }
