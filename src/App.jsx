b9ab00ef6c6af247fe01fed12231251dde8ccbce
Stefanos Gavouchidis <129869539+StefGvxds@users.noreply.github.com>
2024-05-20 14:46:25 +0300

    ads

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import Player from "./components/Player.jsx";
 import { useState } from "react";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curPlayer) => (curPlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
-      <div id="game-container">
+      <div id="game-container" className="highlight-player">
         <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
         <h1>Player</h1>
-        <p>Game Board</p>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
     </main>
   );
